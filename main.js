1a75b8cc0e0666351671031d8c856d0ce1776cdf
Janisha876 <94450884+Janisha876@users.noreply.github.com>
2022-04-04 19:41:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+status="";
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status: detecting objects";
+    object_name=document.getElementById("name").value;
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+}
+function draw(){
+    image(video,0,0,380,380);
+}
